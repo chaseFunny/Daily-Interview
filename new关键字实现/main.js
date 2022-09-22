454fa01bf1b0523befcc56ca627c7c344317a707
zhanghao <3074994545@qq.com>
2022-09-22 22:46:05 +0800

    8-17：解构赋值

--- new关键字实现/main.js
+++ new关键字实现/main.js
@@ -41,7 +41,7 @@ function new_object() {
 // 更优解
 function create() {
   // 1、获得构造函数，同时删除 arguments 中第一个参数
-  Con = [].shift.call(arguments);
+  let Con = [].shift.call(arguments);
   // 2、创建一个空的对象并链接到原型，obj 可以访问构造函数原型中的属性
   let obj = Object.create(Con.prototype);
   // 3、绑定 this 实现继承，obj 可以访问到构造函数中的属性
