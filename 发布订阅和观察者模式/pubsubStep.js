717ee6256429e2febab4c10be19e5c739ab85837
snailrun <3074994545@qq.com>
2025-04-06 17:01:45 +0800

    feat: 实现基本订阅功能

--- 发布订阅和观察者模式/pubsubStep.js
+++ 发布订阅和观察者模式/pubsubStep.js
@@ -6,4 +6,30 @@ class PubSub {
     this.topics = new Map();
     this.subUid = -1; // 订阅 id
   }
+  /**
+   * 订阅者
+   * @param {*} topic 订阅主题
+   * @param {*} callback 执行响应
+   * @returns 订阅 token，用于取消订阅
+   */
+  subscribe(topic, callback) {
+    // 如果还没有该主题
+    if (!this.topics.has(topic)) {
+      this.topics.set(topic, []);
+    }
+    // 生成唯一订阅 id
+    const token = (++this.subUid).toString();
+    // 存储订阅者
+    this.topics.get(topic).push({
+      token,
+      callback,
+    });
+    return token;
+  }
 }
+
+// 使用示例
+const pubSub = new PubSub();
+pubSub.subscribe("sayHei", () => {
+  console.log("hi");
+});
